a8194e0178e2196f271d584ac2f58a71e2457606
RdeRausseo <amrausseo20@gmail.com>
2025-05-17 14:51:47 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function () {
+  //write your code here
+
+  function generateExcuse() {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    let excusa = document.getElementById("excusa")
+
+    excusa.innerHTML = who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)] + " " + 
+                      what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)]
+
+    return excusa; 
+  }
+  generateExcuse(); 
+
+};
